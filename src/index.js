f0193190bab7206e424bfe23486e01b028a228c1
Siu <siuc@protonmail.com>
2022-04-16 16:16:08 -0400

    set up reducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import rootReducer from "./reducuers/";
+
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
---
+++ src/reducuers/index.js
@@ -0,0 +1,7 @@
+import { initState } from "../constants";
+
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
