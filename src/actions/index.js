6c7e951c76d40eb2b514b3fd48f797b202e4a528
Siu <siuc@protonmail.com>
2022-04-16 18:54:48 -0400

    able to load all repo from a user

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import {
   SUMBIT_USERNAME,
   STORE_DATA,
   TO_LOADING_STATE,
+  STOP_LOADING_STATE,
 } from "../constants";
 import axios from "axios";
 
@@ -14,14 +15,30 @@ export const submitUsername = () => {
   return { type: SUMBIT_USERNAME };
 };
 
-export const fetchData = (username) => {
-  return async (dispatch) => {
-    const url = `/users/${username}/repos`;
+const fetchMorePage = async (dispatch, username, pageNum, length) => {
+  if (length === 100) {
+    const url = `/users/${username}/repos?sort=created&per_page=100&page=${pageNum}`;
     try {
       dispatch({ type: TO_LOADING_STATE });
       const { data } = await axios.get(url);
-      console.log(data);
       dispatch({ type: STORE_DATA, username, data });
+      fetchMorePage(dispatch, username, pageNum + 1, data.length);
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    dispatch({ type: STOP_LOADING_STATE });
+  }
+};
+
+export const fetchData = (username) => {
+  return async (dispatch) => {
+    const url = `/users/${username}/repos?sort=created&per_page=100`;
+    try {
+      dispatch({ type: TO_LOADING_STATE });
+      const { data } = await axios.get(url);
+      dispatch({ type: STORE_DATA, username, data });
+      fetchMorePage(dispatch, username, 2, data.length);
     } catch (e) {
       console.log(e);
     }
--- src/reducuers/index.js
+++ src/reducuers/index.js
@@ -4,6 +4,7 @@ import {
   SUMBIT_USERNAME,
   STORE_DATA,
   TO_LOADING_STATE,
+  STOP_LOADING_STATE,
 } from "../constants";
 import { getNewUsername, getNewUsers } from "../helpers";
 
@@ -17,9 +18,11 @@ const rootReducer = (state = initState, action) => {
       return { ...state, username, input: "" };
     case STORE_DATA:
       const users = getNewUsers(action.data, action.username, state.users);
-      return { ...state, users, loading: false };
+      return { ...state, users };
     case TO_LOADING_STATE:
       return { ...state, loading: true };
+    case STOP_LOADING_STATE:
+      return { ...state, loading: false };
     default:
       return state;
   }
