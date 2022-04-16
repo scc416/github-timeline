277a5d54359ce1feddc7b1496f3e55f93a874572
Siu <siuc@protonmail.com>
2022-04-16 19:01:14 -0400

    able to store more data

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,11 @@
 export const getNewUsername = (input) => input.trim().toLowerCase();
 
 export const getNewUsers = (data, username, users) => {
-  const formattedData = data.map(
-    ({ id, name, created_at, html_url, description }) => {
+  const existingData = users[username] || [];
+  const formattedData = existingData.concat(
+    data.map(({ id, name, created_at, html_url, description }) => {
       return { id, name, description, created: created_at, url: html_url };
-    }
+    })
   );
   return { ...users, [username]: formattedData };
 };
