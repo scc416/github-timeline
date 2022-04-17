f08845a6540c9617acb29602f8e46aeb3a2e9f53
Siu <siuc@protonmail.com>
2022-04-17 16:08:14 -0400

    add icon

--- src/components/Header/UsernameForm.jsx
+++ src/components/Header/UsernameForm.jsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateInput, submitUsername, removeUsernameError } from "actions";
 import { useEffect } from "react";
 import { errorTimeout } from "constants";
+import Icon from "@mdi/react";
+import { mdiMagnify } from "@mdi/js";
 
 const UsernameForm = () => {
   const dispatch = useDispatch();
@@ -17,23 +19,29 @@ const UsernameForm = () => {
       );
       return () => clearTimeout(hideUsernameError);
     }
-  // eslint-disable-next-line
+    // eslint-disable-next-line
   }, [usernameError]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log()
+    console.log();
     dispatch(submitUsername());
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => dispatch(updateInput(e.target.value))}
-        placeholder="Search GitHub Username"
-      />
+      <div className="input">
+        <Icon
+          path={mdiMagnify}
+          size={1}
+        />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => dispatch(updateInput(e.target.value))}
+          placeholder="Search GitHub Username"
+        />
+      </div>
       {usernameError}
     </form>
   );
